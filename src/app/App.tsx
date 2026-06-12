45b7b5981baac723671a74d2ea11e285e1e8f5b9
Vlad Rymarchik <vladrymarchik@gmail.com>
2026-06-12 19:39:59 +0300

    add: pricing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import FeaturePreview from "./components/FeaturePreview";
+import Pricing from "./components/Pricing";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
       <Hero />
       <FeaturePreview />
+      <Pricing />
     </div>
   );
 }
